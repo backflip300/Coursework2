6bce28b75afa823d819209bdce1ca1c8b0000c54
j00721 <EdwardSmart98@gmail.co.uk>
2016-03-16 10:06:22 +0000

    - building the delete stock

--- JewellryTimetable/src/processing/DeleteStock.java
+++ JewellryTimetable/src/processing/DeleteStock.java
@@ -3,6 +3,8 @@ package processing;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
 import tableModels.StockTableModel;
 
 public class DeleteStock {
@@ -10,23 +12,39 @@ public class DeleteStock {
 	private ExtractProduct pExtractor = new ExtractProduct();
 	FileAccess stocksFile = new FileAccess(Paths.get("TextFiles/Stocks.txt"));
 	ArrayList<String> stocks = new ArrayList<String>();
-
+	ArrayList<String> usingProducts = new ArrayList<String>();
+	private String toDelete;
+	private boolean used, exists;
+	private S
 	public DeleteStock() {
-		
-		
+
 	}
 
-	public int delete(StockTableModel table) {
-		int deletestate = 0;
+	public void delete(StockTableModel table) {
+
+		used = false;
+		stocks = stocksFile.sReadFileData();
+		toDelete = JOptionPane.showInputDialog("Enter name of stock to delete");
 		products = pExtractor.ExtractAll();
-		for ( int i = 0; i < products.length; i++){
-			for(int x = 0; x < products[i].stocks.length;x++){
-				if ()
-					
-				
+
+		Search: for (Product product : products) {
+			for (String stock : product.stocks) {
+				if (stock.equalsIgnoreCase(toDelete)) {
+					used = true;
+					break Search;
+				}
+			}
+
+		}
+
+		if (used == false) {
+			for (int i = 0;i < stocks.size();stocks++) {
+				if (stocks.get(i).equalsIgnoreCase(toDelete)) {
+					lineToDelete = i;
+				}
+
 			}
 		}
-		return deletestate;
 	}
 
 }
